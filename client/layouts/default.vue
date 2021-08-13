59b613780951ebb31da7e3ea4706cc4b24558717
Cheikh THIAM <cheikhthiam95g@gmail.com>
2021-08-13 12:44:01 +0200

    Correction sur le style et Ajout du header au default Layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
+    <!-- header section starts  -->
+
+    <header>
+      <div id="menu-bar" class="fas fa-bars"></div>
+
+      <a href="#" class="logo"><span>T</span>ravel</a>
+
+      <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#book">book</a>
+        <a href="#packages">packages</a>
+        <a href="#services">services</a>
+        <a href="#gallery">gallery</a>
+        <a href="#review">review</a>
+        <a href="#contact">contact</a>
+      </nav>
+
+      <div class="icons">
+        <i class="fas fa-search" id="search-btn"></i>
+        <i class="fas fa-user" id="login-btn"></i>
+      </div>
+
+      <form action="" class="search-bar-container">
+        <input type="search" id="search-bar" placeholder="search here..." />
+        <label for="search-bar" class="fas fa-search"></label>
+      </form>
+    </header>
+
+    <!-- header section ends -->
     <Nuxt />
   </div>
 </template>
